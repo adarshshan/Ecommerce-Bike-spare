8cd99641d8f2c098b6641d52674d8aea46588ab9
Adarsh <adarshshanu3@gmail.com>
2023-10-06 23:07:39 +0530

    deleted some unwanted file

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-app.get('/home',(req,res)=>{
-    res.send('hello how are you? ')
-})
+console.log('hellooo')
 
 app.listen(port,()=>console.log('server is running on port 3000'));
