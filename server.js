cc06cc27e8ad004d97d5a821ea56f8db739c2930
Adarsh <adarshshanu3@gmail.com>
2023-10-06 23:49:12 +0530

    Day-one

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-console.log('hellooo')
+require('dotenv/config')
 
+const api = process.env.API_URL
+
+app.get(`${api}/product`,(req,res)=>{
+    const product={
+        id:1,
+        name:"knife",
+        price:288
+
+    }
+    res.send(product)
+})
+app.post(`${api}/product`,(req,res)=>{
+    const newproduct=req.body;
+    console.log(newproduct)
+    res.send(newproduct)
+})
 app.listen(port,()=>console.log('server is running on port 3000'));
