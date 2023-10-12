eb096e582de96ec042fb702c7e94ac4f37327cb0
Adarsh <adarshshanu3@gmail.com>
2023-10-13 00:41:58 +0530

    lastday

--- routers/user/person.js
+++ routers/user/person.js
@@ -11,5 +11,29 @@ router.get('/',async (req,res)=>{
         }
    
 })
+router.get('/signup',(req,res)=>{
+    res.render('user/signup',{title:'user signUp'})
+})
+router.get('/login',(req,res)=>{
+    res.render('user/login',{title:'User Login'})
+})
+router.get('/productDetails/:id',async (req,res)=>{
+    let id=req.params.id;
+    Products.findById(id).then((products)=>{
+        console.log(products)
+        res.render('user/product_details',{title:'Details',products})
+    }).catch((err)=>{
+        res.redirect('/persons')
+        console.log(err)
+        console.log('Error is at get productDetails')
+    })
+    // let productList=await Products.findOne({_id:id})
+    // if(!productList){
+    //     res.status(500).json({success:false})
+    //     }else{
+    //         res.render('user/product_details',{title:'Details',products:productList})
+    //     }
+    
+})
 
 module.exports=router
