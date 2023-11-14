2d06b476e8430283de29cc71e3f4b5a62dc159c4
Adarsh <adarshshanu3@gmail.com>
2023-11-14 22:07:23 +0530

    coupons

--- models/coupon.js
+++ models/coupon.js
@@ -1,8 +1,25 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const couponSchema=new mongoose.Schema({
-
+const couponSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    expireDate: {
+        type: String,
+        require: true
+    },
+    isActive: {
+        type: Boolean,
+        require: true,
+        default: true
+    }
 })
 
-const coupon=mongoose.model('coupon',couponSchema)
-module.exports=coupon
+const coupon = mongoose.model('coupon', couponSchema)
+module.exports = coupon
--- routers/admin/coupons.js
+++ routers/admin/coupons.js
@@ -1,16 +1,66 @@
-const express=require('express')
-const router=express.Router()
-const Coupon=require('../../models/coupon')
+const express = require('express')
+const router = express.Router()
+const Coupon = require('../../models/coupon')
 
 
-router.get('/',async (req,res)=>{
-    const couponList=await Coupon.find()
+router.get('/', async (req, res) => {
+    const couponList = await Coupon.find()
 
-    res.send(couponList)
+    res.render('admin/coupon.ejs',{title:'Coupon Management',couponList})
 })
 
-router.post('/',(req,res)=>{
+router.post('/', (req, res) => {
+const {amount,expireDate}=req.body
+    let isExistDiscount = false
+    do {
+        let myDiscountCode = coupongenerator()
+        console.log(`myDiscountCode is ${myDiscountCode}`);
+        let newDiscountCode = new Coupon({
+            code: myDiscountCode,
+            isPercent: false,
+            amount: amount,
+            expireDate: expireDate,
+            isActive: true
+        })
+        newDiscountCode.save().then((response)=>{
+            console.log('Coupon crated successfully...');
+            console.log(response);
+            res.redirect('/coupons')
+        }).catch((err)=>{
+            if (err.name === 'MongoError' && err.code === 11000) {
+                // Duplicate code detected
+                console.log('Duplicate code detected');
+                isExistDiscount = true;
+            }
+        })
+    }
+    while (isExistDiscount);
+})
+router.get('/edit-coupon/:id/:amount/:date',async (req,res)=>{
+    const couponId=req.params.id
+    const amount=req.params.amount
+    const expireDate=req.params.date
+    const updated=await Coupon.findByIdAndUpdate(couponId,{$set:{amount:amount,expireDate:expireDate}});
+    if(updated && updated!==undefined){
+        console.log('coupon updated');
+        return res.json({success:true,message:'Coupon updated successfully.'})
+    }else{
+        console.log('Failed to update  coupon');
+        return res.json({success:false,message:'Failed to update  coupon'})
+    }
+
 
 })
 
-module.exports=router;
+module.exports = router;
+
+//additional functions
+
+function coupongenerator() {
+    var coupon = '';
+    var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 6; i++) {
+        coupon += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return coupon;
+}
